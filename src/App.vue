2eb7d8dee5f5697ca534874a3b909b0618996877
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2024-02-05 15:54:57 +0100

    :sparkles: Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,41 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="dark">
+    <div v-if="!currentView.name || currentView.name !== 'NotFound'">
+      <NavBar />
+      <div class="p-4 sm:ml-64">
+        <component :is="currentView" />
+      </div>
+    </div>
+    <component v-else :is="currentView" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import NavBar from './components/NavBar.vue'
+import NotFound from './pages/NotFound.vue'
+import routes from './routes.js'
+
+
+
+export default {
+  data() {
+    return {
+      currentPath: window.location.pathname
+    }
+  },
+  components: {
+    NavBar
+  },
+  computed: {
+    currentView() {
+      const route = routes.find(r => r.path === this.currentPath)
+      return route ? route.component : NotFound
+    }
+  },
+  mounted() {
+    window.addEventListener('hashchange', () => {
+		  this.currentPath = window.location.hash
+		})
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+</script>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,9 @@
+<template>
+  Hello
+</template>
+
+<script>
+export default {
+  name: 'HomePage'
+}
+</script>
