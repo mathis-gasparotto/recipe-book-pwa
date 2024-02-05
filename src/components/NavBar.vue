30b784610626b6b73014b8b7405c1b944a9c7e4e
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2024-02-05 16:57:04 +0100

    :construction: Recipe Details

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,17 +4,19 @@
     aria-label="Sidebar">
     <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
       <ul class="space-y-2 font-medium">
-        <li v-for="route in routes" :key="route.path">
-          <router-link :to="route.path"
-            class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-            <svg v-if="route.svgPaths"
-              class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-              aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
-              <path v-for="path in route.svgPaths" :key="path" :d="path" />
-            </svg>
-            <span class="ms-3">{{ route.name }}</span>
-          </router-link>
-        </li>
+        <template v-for="route in routes" :key="route.path">
+          <li v-if="route.toNavbar">
+            <router-link :to="route.path"
+              class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+              <svg v-if="route.svgPaths && route.svgViewBox"
+                class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" :viewBox="route.svgViewBox">
+                <path v-for="path in route.svgPaths" :key="path" :d="path" />
+              </svg>
+              <span class="ms-3">{{ route.name }}</span>
+            </router-link>
+          </li>
+        </template>
       </ul>
     </div>
   </aside>
@@ -22,16 +24,18 @@
   <div
     class="sm:translate-y-16 transition-transform fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
     <div class="flex justify-evenly h-full max-w-lg mx-auto font-medium">
-      <router-link :to="route.path" v-for="route in routes" :key="route.path"
-        class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
-        <svg v-if="route.svgPaths"
-          class="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
-          aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-          <path v-for="path in route.svgPaths" :key="path" :d="path" />
-        </svg>
-        <span
-          class="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">{{ route.name }}</span>
-      </router-link>
+      <template  v-for="route in routes" :key="route.path">
+        <router-link :to="route.path" v-if="route.toNavbar"
+          class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
+          <svg v-if="route.svgPaths && route.svgViewBox"
+            class="w-5 h-5 mb-2 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+            aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" :viewBox="route.svgViewBox">
+            <path v-for="path in route.svgPaths" :key="path" :d="path" />
+          </svg>
+          <span
+            class="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">{{ route.name }}</span>
+        </router-link>
+      </template>
     </div>
   </div>
 </template>
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import HomePage from './pages/HomePage.vue'
 import ListeCoursesPage from './pages/ListeCoursesPage.vue'
 import RecipeListPage from './pages/RecipeListPage.vue'
+import RecipeDetailsPage from './pages/RecipeDetailsPage.vue'
 
 const routes = [
   {
@@ -9,15 +10,25 @@ const routes = [
     name: 'Home',
     svgPaths: [
       'm19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z'
-    ]
+    ],
+    svgViewBox: '0 0 20 20',
+    toNavbar: true
   },
   {
     path: '/recipes',
     component: RecipeListPage,
     name: 'Recipes',
     svgPaths: [
-      'm19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z'
-    ]
+      'M50 2H16a6.007 6.007 0 0 0-6 6v42a6.007 6.007 0 0 0 6 6h3v5a1 1 0 0 0 1.707.707L24 58.414l3.293 3.293A1 1 0 0 0 29 61v-5h21a4.004 4.004 0 0 0 4-4V6a4.004 4.004 0 0 0-4-4ZM16 4h34a2.002 2.002 0 0 1 2 2v38H16a5.969 5.969 0 0 0-4 1.54V8a4.004 4.004 0 0 1 4-4Zm8.707 52.293a1 1 0 0 0-1.414 0L21 58.586V51h6v7.586ZM50 54H29v-3h19a1 1 0 0 0 0-2H17a1 1 0 0 0 0 2h2v3h-3a4 4 0 0 1 0-8h36v6a2.002 2.002 0 0 1-2 2ZM21 28.411V36a3.003 3.003 0 0 0 3 3h16a3.003 3.003 0 0 0 3-3v-7.589a7.99 7.99 0 0 0-4.034-15.332 7.987 7.987 0 0 0-13.932 0A7.99 7.99 0 0 0 21 28.411ZM40 37H24a1.001 1.001 0 0 1-1-1v-1h18v1a1.001 1.001 0 0 1-1 1ZM24.26 15.016A7.962 7.962 0 0 0 24 17a1 1 0 0 0 2 0 6 6 0 0 1 12 0 1 1 0 0 0 2 0 7.961 7.961 0 0 0-.26-1.984 5.995 5.995 0 1 1-1.453 11.731 1 1 0 0 0-.574 1.916 7.706 7.706 0 0 0 3.287.268V33H23v-4.07a7.706 7.706 0 0 0 3.287-.267 1 1 0 0 0-.574-1.916 5.994 5.994 0 1 1-1.454-11.73Z'
+    ],
+    svgViewBox: '0 0 64 64',
+    toNavbar: true
+  },
+  {
+    path: '/recipes/:id',
+    component: RecipeDetailsPage,
+    name: 'Recipe Details',
+    toNavbar: false
   },
   {
     path: '/listecourses',
