3e699debb6cce5031ad4e05202305d4218d95a28
jadegurnaud <jade.gurnaud@gmail.com>
2024-02-27 16:40:07 +0100

    var .env

--- src/service-worker.js
+++ src/service-worker.js
@@ -6,13 +6,13 @@ cleanupOutdatedCaches()
 precacheAndRoute(self.__WB_MANIFEST)
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDUoxdnUNtCEqDhiFGMzQN5p7MLzMLOecw",
-    authDomain: "recipe-book-pwa-b837b.firebaseapp.com",
-    projectId: "recipe-book-pwa-b837b",
-    storageBucket: "recipe-book-pwa-b837b.appspot.com",
-    messagingSenderId: "789256533856",
-    appId: "1:789256533856:web:38998f11548961611b1005",
-    measurementId: "G-53DP3X17W3"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig)
