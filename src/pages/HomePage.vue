057fe6bc3888605ca5b520460f06525dc9cdf19b
jadegurnaud <jade.gurnaud@gmail.com>
2024-02-28 13:34:35 +0100

    signin front

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,51 +1,61 @@
 <template>
-  <div>
-    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Télécharger un fichier</label>
-    <input @change="handleFileUpload" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file">
-
-    <textarea v-model="resultText" class="mt-4 h-52 w-full p-2 border border-gray-300 rounded-lg text-gray-900 dark:text-white bg-gray-50 dark:bg-gray-700 focus:outline-none" placeholder="Tapez votre texte ou téléchargez un fichier"></textarea>
-  </div>
+  <section class="bg-gray-50 dark:bg-gray-900">
+    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+            <img class="w-8 h-8 mr-2" src="/icons/icon.png" alt="logo">
+            Book Recipe    
+        </a>
+        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                    Sign in to your account
+                </h1>
+                <form ref="loginform" class="space-y-4 md:space-y-6" @submit.prevent="onsubmit()">
+                    <div>
+                        <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                        <input type="username" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
+                    </div>
+                    <div>
+                        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                        <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                    </div>
+                    <div class="flex items-center justify-between">
+                        <div class="flex items-start">
+                            <div class="flex items-center h-5">
+                              <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="">
+                            </div>
+                            <div class="ml-3 text-sm">
+                              <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
+                            </div>
+                        </div>
+                        <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                    </div>
+                    <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                    <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                        Don’t have an account yet? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                    </p>
+                </form>
+            </div>
+        </div>
+    </div>
+  </section>
 </template>
 
 <script>
-import Tesseract from 'tesseract.js';
 
 export default {
   name: 'HomePage',
   data() {
     return {
-      resultText: ''
+      form: {
+        email: '',
+        password: '',
+        rememberMe: false,
+      },
+      showPassword: false,
+      loading: false,
+      validate: false
     };
   },
-  methods: {
-    // Fonction appelée lorsqu'un fichier est sélectionné
-    handleFileUpload(event) {
-      const fileInput = event.target;
-      if (fileInput.files.length > 0) {
-        const uploadedImage = fileInput.files[0];
-        this.processImage(uploadedImage);
-      }
-    },
-    // Fonction pour traiter l'image avec Tesseract.js
-    processImage(image) {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        const imageData = e.target.result;
-
-        Tesseract.recognize(imageData, 'fra', {
-          logger: m => console.log(m)
-        })
-        .then(({ data: { text } }) => {
-          console.log(text);
-
-          this.resultText = text;
-        })
-        .catch(error => {
-          console.error('Erreur lors du traitement de l\'image:', error);
-        });
-      };
-      reader.readAsDataURL(image);
-    }
-  }
 }
 </script>
