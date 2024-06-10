d71c63866f135e9d4127069532d7e3774a530e47
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2024-06-10 15:26:38 +0200

    feat: login

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -10,19 +10,19 @@
                 <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                     Sign in to your account
                 </h1>
-                <form ref="loginform" class="space-y-4 md:space-y-6" @submit.prevent="onsubmit()">
+                <form ref="loginform" class="space-y-4 md:space-y-6" @submit.prevent="onSubmit()">
                     <div>
                         <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                        <input type="username" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
+                        <input type="username" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" v-model="form.email">
                     </div>
                     <div>
                         <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                        <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                        <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" v-model="form.password">
                     </div>
                     <div class="flex items-center justify-between">
                         <div class="flex items-start">
                             <div class="flex items-center h-5">
-                              <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="">
+                              <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" v-model="form.rememberMe">
                             </div>
                             <div class="ml-3 text-sm">
                               <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
@@ -43,6 +43,7 @@
 
 <script>
 import logo from '../assets/icon.png'
+import { registerUser, loginUser } from '../services/firebaseService'
 
 export default {
   name: 'HomePage',
@@ -59,9 +60,21 @@ export default {
         rememberMe: false,
       },
       showPassword: false,
-      loading: false,
-      validate: false
+      loading: false
     };
   },
+  methods: {
+    onSubmit() {
+      this.loading = true
+      loginUser(this.form.email, this.form.password)
+        .then(() => {
+          this.loading = false
+          this.$router.push('/recipes')
+        })
+        .catch(() => {
+          this.loading = false
+        })
+    }
+  }
 }
 </script>
